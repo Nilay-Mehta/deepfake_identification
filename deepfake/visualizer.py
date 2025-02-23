25c5db4936905091866961c857b59aca040e8cb4
Nilay <145849812+Nilay-Mehta@users.noreply.github.com>
2025-02-23 12:15:03 +0530

    Updating colors
    The colors for the rectangle around face and text was changed to cyan

--- deepfake/visualizer.py
+++ deepfake/visualizer.py
@@ -22,15 +22,15 @@ class Visualizer:
             y2 = min(output.shape[0], y + h + y_pad)   
             
             # Draw rectangle around face
-            cv2.rectangle(output, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(output, (x1, y1), (x2, y2), (255, 255, 0), 2)
             
             # Add confidence label
             cv2.putText(output, f"{prediction:.2f}", (x2 - 15, max(10, y1 - 10)),
-                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
             
             prediction = "Real" if prediction > conf else "Fake"
             cv2.putText(output, str(prediction), (x1, max(10, y1 - 10)),
-                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)            
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)            
                        
         return output
         
